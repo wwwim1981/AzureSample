d0f97e07934e76facec08deeb00b1c4658de6e07
wwwim1981 <wwwim1981@yahoo.co.jp>
2019-02-21 23:34:21 +0900

    commit

---
+++ 01.IOT HUB/01.AndroidAppSample/app/build/generated/source/buildConfig/debug/com/microsoft/azure/iot/sdk/samples/androidsample/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.microsoft.azure.iot.sdk.samples.androidsample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.iothub.azure.microsoft.com.androidsample";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String DeviceConnectionString = "HostName=wwwTestHub.azure-devices.net;DeviceId=MyTestDevice;SharedAccessKey=sVfpJOu7LDg07o0lT6QGpLhSsRAIQNc4cQWJQvgkf7w=";
+}
